58820e4805ba6b6038f6345a614f26a78b9ac5e1
Yves Lima Silva <yveslimasilva@gmail.com>
2021-11-30 13:28:59 -0300

    …do cliente e implementação de sutis animações
    Dei funcionalidade para o select de seleção dos chamados por categoria, tal como uma sutil animação caso desse erro ao realizar a busca; e animei o erro de login e inscrição.

--- App/route.php
+++ App/route.php
@@ -74,6 +74,12 @@
                 'controller' => 'AppController',
                 'action' => 'consultar_cliente_adm'
             );
+
+            $routes['consultar_categoria_adm'] = array(
+                'route' => '/consultar_categoria_adm',
+                'controller' => 'AppController',
+                'action' => 'consultar_categoria_adm'
+            );
     
             $routes['sair'] = array(
                 'route' => '/sair',
